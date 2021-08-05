e733d66bea692ff773755789f3345f5abdf79b43
DESKTOP-NCBS2K5\wenwen <1787742075@qq.com>
2021-08-05 13:31:41 +0800

    添加功能

--- pages/addTask/addTask.js
+++ pages/addTask/addTask.js
@@ -5,7 +5,60 @@ Page({
    * 页面的初始数据
    */
   data: {
+    showTopTips: false,
 
+    radioItems: [
+        {name: 'cell standard', value: '0', checked: true},
+        {name: 'cell standard', value: '1'}
+    ],
+    checkboxItems: [
+        {name: 'standard is dealt for u.', value: '0', checked: true},
+        {name: 'standard is dealicient for u.', value: '1'}
+    ],
+    items: [
+        {name: 'USA', value: '美国'},
+        {name: 'CHN', value: '中国', checked: 'true'},
+        {name: 'BRA', value: '巴西'},
+        {name: 'JPN', value: '日本'},
+        {name: 'ENG', value: '英国'},
+        {name: 'TUR', value: '法国'},
+    ],
+
+    date: "2016-09-01",
+    time: "12:01",
+
+    countryCodes: ["+86", "+80", "+84", "+87"],
+    countryCodeIndex: 0,
+
+    countries: ["中国", "美国", "英国"],
+    countryIndex: 0,
+
+    accounts: ["微信号", "QQ", "Email"],
+    accountIndex: 0,
+
+    isAgree: false,
+    formData: {
+
+    },
+    rules: [{
+        name: 'radio',
+        rules: {required: true, message: '单选列表是必选项'},
+    }, {
+        name: 'checkbox',
+        rules: {required: true, message: '多选列表是必选项'},
+    }, {
+        name: 'qq',
+        rules: {required: true, message: 'qq必填'},
+    }, {
+        name: 'mobile',
+        rules: [{required: true, message: 'mobile必填'}, {mobile: true, message: 'mobile格式不对'}],
+    }, {
+        name: 'vcode',
+        rules: {required: true, message: '验证码必填'},
+    }, {
+        name: 'idcard',
+        rules: {required: true, message: 'idcard必填'},
+    }]
   },
 
   /**
